6334547d52971f45207a33fd619e4c817c9cb4b5
klos593 <agustinkloster59@gmail.com>
2025-10-22 17:47:25 -0300

    creacion endpoints para profesionales y profesiones

--- src/controllers/professionals.controller.ts
+++ src/controllers/professionals.controller.ts
@@ -36,3 +36,44 @@ export async function getProfessionals() {
 
   return formattedProfessionals;
 }
+
+export async function getProfessionalById(professionalId: number) {
+  const professionalFromDb = await prisma.user.findUniqueOrThrow({
+      where: {
+        id: professionalId,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        lastName: true,
+        phone: true,
+        rating: true,
+        picture: true,
+        description: true,
+        UserProfession: {
+          select: {
+            profession: {
+              select: {
+                name: true,
+              },
+            },
+          },
+        },
+      },
+    });
+
+    const formattedProfessional = {
+      id: professionalFromDb.id,
+      mail: professionalFromDb.email, 
+      name: professionalFromDb.name,
+      lastName: professionalFromDb.lastName,
+      phoneNumber: professionalFromDb.phone, 
+      professions: professionalFromDb.UserProfession.map(up => up.profession.name),
+      rating: Number(professionalFromDb.rating), 
+      picture: professionalFromDb.picture,
+      description: professionalFromDb.description,
+    };
+    
+    return formattedProfessional;
+}
--- src/routes/professional.route.ts
+++ src/routes/professional.route.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
-import { getProfessionals } from "../controllers/professionals.controller.js";
+import { getProfessionalById, getProfessionals } from "../controllers/professionals.controller.js";
 
 const professionalRouter = Router();
 
 professionalRouter.get("/professionals", async (req, res) => {
     const allProfessionals = await getProfessionals();
     res.json(allProfessionals);
+});
+
+professionalRouter.get("/professional/:id", async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).json({ error: "Missing id parameter" });
+    }
+    const professionalId = parseInt(id, 10);
+    if (Number.isNaN(professionalId)) {
+        return res.status(400).json({ error: "Invalid id parameter" });
+    }
+    const professional = await getProfessionalById(professionalId);
+    res.json(professional);
 });
 
 export default professionalRouter;
